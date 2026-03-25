1f9aaccffb793cd8ac87a17d6d7edd16decf7f04
unawarexi <unawarexi@gmail.com>
2026-03-25 17:39:29 +0100

    feat(admin): add KYC verification module
    - Add KycVerification overview, PendingKyc, VerifiedKyc
    - Add RejectedKyc, EnhancedKyc components

---
+++ Frontend-Nordi-Remittance/src/pages/admin/app/kyc/RejectedKyc.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import KycVerification from "./KycVerification";
+
+export default function RejectedKyc() {
+  return <KycVerification />;
+}
---
+++ Frontend-Nordi-Remittance/src/pages/admin/app/kyc/VerifiedKyc.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import KycVerification from "./KycVerification";
+
+export default function VerifiedKyc() {
+  return <KycVerification />;
+}
